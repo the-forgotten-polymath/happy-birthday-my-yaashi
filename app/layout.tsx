cf7a5f78d1acc6e19d5442e8ebd884fb94cc5f9a
Chitransh <dev@Developers-MacBook-Air.local>
2026-08-21 00:02:35 +0530

    Fix page title to 18th

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ const caveat = Caveat({
 });
 
 export const metadata: Metadata = {
-  title: `Happy 22nd Birthday, ${config.name} 🎉`,
+  title: `Happy 18th Birthday, ${config.name} 🎉`,
 };
 
 export const viewport: Viewport = {
